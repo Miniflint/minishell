55a8fef8aa87cf63132518f12332cbf852813845
Tristan <tristan@MacBook-de-Leo.local>
2024-12-12 09:09:13 +0100

    SHLVL WORKS

--- srcs/init/init_shell.c
+++ srcs/init/init_shell.c
@@ -12,10 +12,52 @@
 
 #include "minishell.h"
 
+char *ft_strjoin_free(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*new_str;
+
+	new_str = (char *)malloc(sizeof(char) *
+		(ft_strlen(s1) + ft_strlen(s2) + 1));
+	i = 0;
+	while (s1[i])
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		new_str[i++] = s2[j++];
+	new_str[i] = 0;
+	return (new_str);
+}
+
+void	change_env_str(void)
+{
+	char	**env_str;
+	char	*str;
+
+	env_str = ft_get_str_env();
+	while (*env_str)
+	{
+		if (ft_strncmp(*env_str, "SHLVL=", 6) == 0)
+			break ;
+		(*env_str)++;
+	}
+	str = *env_str;
+	while (*str && *str != '=')
+		str++;
+	str++;
+	str = ft_strjoin_free("SHLVL=", ft_itoa(ft_atoi(str) + 1));
+	*env_str = str;
+}
+
 void	init_shell(t_shell *shell, char **m_env)
 {
-	shell->env = init_env(m_env);
-	shell->export = init_export();
 	shell->str_env = m_env;
+	change_env_str();
+	shell->env = init_env(shell->str_env);
+	shell->export = init_export();
 	shell->if_sig = 1;
 }
