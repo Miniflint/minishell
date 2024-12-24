7aa82c92e7afcdb4e5e6f6da428d7c45a5c0e903
Tristan <tristan@MacBook-de-Leo.local>
2024-12-24 01:43:53 +0100

    commited

--- srcs/match.c
+++ srcs/match.c
@@ -1,5 +1,6 @@
 #include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
  struct dirent {
@@ -14,6 +15,7 @@ typedef struct dirent s_dir;
 char	*ft_strcat(char *dest, char *src);
 void	ft_strcpy(char *dst, char *src);
 int	ft_strcmp(const char *s1, char *s2);
+char	*ft_strjoin(char *s1, char *s2);
 
 
 int match(char *s1, char *s2)
@@ -31,52 +33,68 @@ int match(char *s1, char *s2)
 	return (0);
 }
 
-void	rec_dir(DIR *dir_ptr, char **sep, int depth, char *base_path, const int max_depth)
+int	skip_add_path(char full_path[1024 + 1], char *d_name, char *base_path, const int dir_or_file)
+{
+	if (!ft_strcmp(d_name, ".") || !ft_strcmp(d_name, ".."))
+		return (1);
+	ft_strcpy(full_path, base_path);
+	ft_strcat(full_path, "/");
+	ft_strcat(full_path, d_name);
+	if (dir_or_file == 1)
+		ft_strcat(full_path, "/");
+	return (0);
+}
+
+void	rec_dir(DIR *dir_ptr, char **sep, int depth, char *base_path, const int max_depth, const int dir_or_file, char **rtn)
 {
 	char	full_path[1024 + 1];
 	DIR	*sub_dir;
 	s_dir	*idk;
+	char	*get_name;
 
 	while ((idk = readdir(dir_ptr)) != NULL)
 	{
-		if (!ft_strcmp(idk->d_name, ".") || !ft_strcmp(idk->d_name, ".."))
-			continue ; 
-		ft_strcpy(full_path, base_path);
-		ft_strcat(full_path, "/");
-		ft_strcat(full_path, idk->d_name);
-		printf("[[%s]]\n", full_path);
+		if (skip_add_path(full_path, idk->d_name, base_path, dir_or_file))
+			continue ;
+		get_name = NULL;
 		if (idk->d_type == DT_DIR)
 		{
-			if (depth < max_depth - 1 && match(sep[depth], (char *)idk->d_name))
+			if ((max_depth == -1 || depth <= max_depth) && match(sep[depth], (char *)idk->d_name))
 			{
 				sub_dir = opendir(full_path);
 				if (!sub_dir)
 					return ;
-				rec_dir(sub_dir, sep, depth + 1, full_path, max_depth);
+				if (dir_or_file == 1)
+					get_name = full_path;
+				rec_dir(sub_dir, sep, depth + 1, full_path, max_depth, dir_or_file, rtn);
 				closedir(sub_dir);
 			}
 		}
-		else if (idk->d_type == DT_REG)
+		if (!dir_or_file && depth == max_depth && match(sep[max_depth], (char *)idk->d_name))
+			get_name = full_path;
+		if (get_name)
 		{
-			if (depth < max_depth - 1 && match(sep[depth], (char *)idk->d_name))
-				printf("REGULAR FILE MATCH:\n\td_name: [%s]\n\n", idk->d_name);
+			char *tmp = ft_strjoin(*rtn, full_path);
+			free(*rtn);
+			*rtn = ft_strjoin(tmp, " ");
+			free(tmp);
 		}
-		else
-			printf("other: d_name:\n\t[%s]\n\n", idk->d_name);
 	}
 }
 
 int	check_open_dir(char *path, char **sep, int size)
 {
 	DIR	*dir_ptr;
+	char	*str;
 
 	if (!path)
 		return (1);
 	dir_ptr = opendir(path);
 	if (!dir_ptr)
 		return (printf("could not open the base dir\n"));
-	rec_dir(dir_ptr, sep, 0, path, size - 2);
+	rec_dir(dir_ptr, sep, 0, path, size, 0, &str);
 	closedir(dir_ptr);
+	printf("%s\n", str);
 	return (0);
 }
 
@@ -84,7 +102,7 @@ int main(int argc, char **argv)
 {
 	if (argc < 3)
 		return (1);
-	check_open_dir(argv[1], argv + 2, argc);
+	check_open_dir(argv[1], argv + 2, argc - 3);
 	//printf("%d\n", match(argv[1], argv[2]));
 	return (0);
 }
--- srcs/util.c
+++ srcs/util.c
@@ -1,3 +1,17 @@
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (i);
+	while (s[i])
+		i++;
+	return (i);
+}
+
 void	ft_strcpy(char *dst, char *src)
 {
 	int	i;
@@ -34,4 +48,23 @@ int	ft_strcmp(const char *s1, char *s2)
 	while (s1[i] && s2[i] && (s1[i] == s2[i]))
 		i++;
 	return (s1[i] - s2[i]);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	char	*ret;
+
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	ret = str;
+	if (!str)
+		return (0);
+	if (s1)
+		while (*s1)
+			*(str++) = *(s1++);
+	if (s2)
+		while (*s2)
+			*(str++) = *(s2++);
+	*str = 0;
+	return (ret);
 }
